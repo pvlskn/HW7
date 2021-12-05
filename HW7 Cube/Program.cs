3172c1df98eb727ae16c3632686152d702819abe
p p <pvlskn@yandex.ru>
2021-12-05 10:53:08 +0300

    Добавьте файлы проекта.

---
+++ HW7 Cube/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Создать метод для вычисления объема и площади поверхности куба по длине его ребра.
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите длину ребра куба: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+        double S = CubeSqu(a);
+        double V = CubeVol(a);
+        Console.WriteLine("Площадь поверхности куба равна {0}.", S);
+        Console.WriteLine("Объём куба равен {0}.", V);
+        Console.WriteLine("Нажмите любую клавишу для выхода.");
+        Console.ReadKey();
+    }
+    static double CubeSqu(double a)
+    {
+        double S = 6 * a * a;
+        return S;
+    }
+    static double CubeVol(double a)
+    {
+        double V = a * a * a;
+        return V;
+    }
+}
---
+++ HW7/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Два треугольника заданы длинами своих сторон. 
+ * Определить, площадь какого из них больше (создать метод для вычисления  площади  треугольника по длинам его сторон).  
+ * Для решения задачи можно использовать формулу Герона */
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите длины трёх сторон первого треугольника: ");
+        double a1 = Convert.ToDouble(Console.ReadLine());
+        double b1 = Convert.ToDouble(Console.ReadLine());
+        double c1 = Convert.ToDouble(Console.ReadLine());
+        double S1 = TriangleSqu(a1, b1, c1);
+        Console.WriteLine("Введите длины трёх сторон второго треугольника: ");
+        double a2 = Convert.ToDouble(Console.ReadLine());
+        double b2 = Convert.ToDouble(Console.ReadLine());
+        double c2 = Convert.ToDouble(Console.ReadLine());
+        double S2 = TriangleSqu(a2, b2, c2);
+        if (S1 > S2)
+        {
+            Console.WriteLine("Площадь первого треугольника больше.");
+        }
+        else if (S2 > S1)
+        {
+            Console.WriteLine("Площадь второго треугольника больше.");
+        }
+        else if (S2 == S1)
+        {
+            Console.WriteLine("Площади треугольников равны.");
+        }
+        Console.WriteLine("Нажмите любую клавишу для выхода.");
+        Console.ReadKey();
+    }
+    static double TriangleSqu(double a, double b, double c)
+    {
+        double P = 0.5 * (a + b + c);
+        double S = Math.Sqrt(P * (P - a) * (P - b) * (P - c));
+        return S;
+    }
+}
